52d09d2742f3ea365928b1e7eba4c0e54a48658c
Midnight <artiksakinbiri@gmail.com>
2025-12-17 23:48:03 +0300

    Add redirect for legacy message routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 // Trigger deploy fix
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/messages/:id',
+        destination: '/messages?startWith=:id',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
